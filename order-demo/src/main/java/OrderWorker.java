98ca0b7d30e99e0077a74d9922673a57e1a12aea
XimCervantesPatlan <ximena.cervantes@ibm.com>
2025-06-18 11:28:01 -0600

    Change of the directory for the Starter and Worker

---
+++ order-demo/src/main/java/OrderWorker.java
@@ -0,0 +1,26 @@
+
+
+import com.ibm.activities.OrderActivitiesImpl;
+import com.ibm.workflows.OrderWorkflowImpl;
+
+import io.temporal.client.WorkflowClient;
+import io.temporal.serviceclient.WorkflowServiceStubs;
+import io.temporal.worker.Worker;
+import io.temporal.worker.WorkerFactory;
+
+public class OrderWorker {
+    public static void main(String[] args){
+        WorkflowServiceStubs service = WorkflowServiceStubs.newLocalServiceStubs();
+
+        WorkflowClient client = WorkflowClient.newInstance(service);
+        WorkerFactory factory = WorkerFactory.newInstance(client);
+
+        Worker worker = factory.newWorker("order-tasks");
+        worker.registerWorkflowImplementationTypes(OrderWorkflowImpl.class);
+        worker.registerActivitiesImplementations(new OrderActivitiesImpl());
+        factory.start();
+        
+        if(factory.isStarted())
+        System.out.println("Worker started successfully: " + factory.getWorker("order-tasks"));
+    }
+}
