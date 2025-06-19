ce216f4ccdbb98da3e7a1820eaf2afd2c85606d7
XimCervantesPatlan <ximena.cervantes@ibm.com>
2025-06-19 17:59:36 -0600

    Changes made to start the workflow environment

--- order-demo/src/test/java/workflows/OrderWorkflowImplTest.java
+++ order-demo/src/test/java/workflows/OrderWorkflowImplTest.java
@@ -17,38 +17,41 @@ import io.temporal.worker.WorkerFactory;
 
 public class OrderWorkflowImplTest {
 
-    private TestWorkflowEnvironment testEnv;
+    private TestWorkflowEnvironment testWorkflowEnvironment;
     private Worker worker;
     private WorkflowClient client;
 
     // Set up the test workflow environment
     @BeforeEach
     public void setUp() {
-        testEnv = TestWorkflowEnvironment.newInstance();
-        WorkerFactory factory = testEnv.getWorkerFactory();
-        worker = testEnv.newWorker("ORDER_TASK_QUEUE");
+        testWorkflowEnvironment = TestWorkflowEnvironment.newInstance();
+        WorkerFactory factory = testWorkflowEnvironment.getWorkerFactory();
+        worker = testWorkflowEnvironment.newWorker("order_tasks");
         // Register your workflow implementations
         worker.registerWorkflowImplementationTypes(OrderWorkflowImpl.class);
         worker.registerActivitiesImplementations(new OrderActivitiesImpl());
-
+        
         factory.start();
-        client = testEnv.getWorkflowClient();
+        client = testWorkflowEnvironment.getWorkflowClient();
     }
 
     // Clean up test environment after tests are completed
     @AfterEach
     public void tearDown() {
-        testEnv.close();
+        testWorkflowEnvironment.close();
     }
 
     @Test
     public void testActivityImpl() {
+        // Start test environment
+        testWorkflowEnvironment.start();
         Order demoOrder = new Order();
         demoOrder.setOrderId("order-1");
         demoOrder.setCardNumber("1239459612508400");
         demoOrder.setClientName("Ximena Cervantes");
-        OrderWorkflow workflow = client.newWorkflowStub(OrderWorkflow.class, WorkflowOptions.newBuilder()
-                                        .setTaskQueue("ORDER_TASK_QUEUE").build());
+        OrderWorkflow workflow = client.newWorkflowStub(OrderWorkflow.class, 
+                                        WorkflowOptions.newBuilder()
+                                        .setTaskQueue("order_tasks").build());
         WorkflowClient.start(workflow::processOrder, demoOrder);
     }
 }
